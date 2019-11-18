b8799c544b9396408990bd743242d8ff6a6c723e
ArunKumar <arunkumar.e@outlook.com>
2019-11-18 12:35:12 +0530

    Created TEstClass on 18/11/2019 @ 12:35pm

---
+++ MovieStore/Models/TestClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieStore.Models
+{
+    public class TestClass
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
